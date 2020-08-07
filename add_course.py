81abd30f59f443d465181fbf48362995252c3b32
bensim602 <bensim.602@gmail.com>
2020-08-07 16:35:43 +0200

    Github pages must be on docs directory (or other branch)

--- add_course.py
+++ add_course.py
@@ -6,7 +6,7 @@ import codecs
 iso639 = ['ab', 'aa', 'af', 'ak', 'sq', 'am', 'ar', 'an', 'hy', 'as', 'av', 'ae', 'ay', 'az', 'bm', 'ba', 'eu', 'be', 'bn', 'bh', 'bi', 'bs', 'br', 'bg', 'my', 'ca', 'ch', 'ce', 'ny', 'zh', 'cv', 'kw', 'co', 'cr', 'hr', 'cs', 'da', 'dv', 'nl', 'dz', 'en', 'eo', 'et', 'ee', 'fo', 'fj', 'fi', 'fr', 'ff', 'gl', 'ka', 'de', 'el', 'gn', 'gu', 'ht', 'ha', 'he', 'hz', 'hi', 'ho', 'hu', 'ia', 'id', 'ie', 'ga', 'ig', 'ik', 'io', 'is', 'it', 'iu', 'ja', 'jv', 'kl', 'kn', 'kr', 'ks', 'kk', 'km', 'ki', 'rw', 'ky', 'kv', 'kg', 'ko', 'ku', 'kj', 'la', 'lb', 'lg', 'li', 'ln', 'lo', 'lt', 'lu', 'lv', 'gv', 'mk', 'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mh', 'mn', 'na', 'nv', 'nb', 'nd', 'ne', 'ng', 'nn', 'no', 'ii', 'nr', 'oc', 'oj', 'cu', 'om', 'or', 'os', 'pa', 'pi', 'fa', 'pl', 'ps', 'pt', 'qu', 'rm', 'rn', 'ro', 'ru', 'sa', 'sc', 'sd', 'se', 'sm', 'sg', 'sr', 'gd', 'sn', 'si', 'sk', 'sl', 'so', 'st', 'es', 'su', 'sw', 'ss', 'sv', 'ta', 'te', 'tg', 'th', 'ti', 'bo', 'tk', 'tl', 'tn', 'to', 'tr', 'ts', 'tt', 'tw', 'ty', 'ug', 'uk', 'ur', 'uz', 've', 'vi', 'vo', 'wa', 'cy', 'wo', 'fy', 'xh', 'yi', 'yo', 'za', 'zu']
 
 # Read the market place file
-with open("Github-Pages/marketplace.json", "r") as f:
+with open("docs/marketplace.json", "r") as f:
     marketplace = json.load(f)
 
 """ Create new course """
@@ -108,13 +108,13 @@ while input("Do you want to add translation ? Yes[Y] or No[N]: ") in ["Yes", "ye
 
 if input("Do you want to validate your new course ? Yes[Y] or No[N]: ") in ["Yes", "yes", "Y", "y"]:
     marketplace.append(course)
-    with codecs.open("Github-Pages/marketplace.json", "w", "utf-8") as f:
+    with codecs.open("docs/marketplace.json", "w", "utf-8") as f:
         content = json.dumps(marketplace, sort_keys=False, indent=4, separators=(',', ': '))
         f.write(content)
 else:
     if input("This will lose what you typed! Are you sure ? Yes[Y] or No[N]: ") not in ["Yes", "yes", "Y", "y"]:
         marketplace.append(course)
-        with codecs.open("Github-Pages/marketplace.json", "w", "utf-8") as f:
+        with codecs.open("docs/marketplace.json", "w", "utf-8") as f:
             content = json.dumps(marketplace, sort_keys=False, indent=4, separators=(',', ': '))
             f.write(content)
 
--- edit_course.py
+++ edit_course.py
@@ -7,7 +7,7 @@ import codecs
 iso639 = ['ab', 'aa', 'af', 'ak', 'sq', 'am', 'ar', 'an', 'hy', 'as', 'av', 'ae', 'ay', 'az', 'bm', 'ba', 'eu', 'be', 'bn', 'bh', 'bi', 'bs', 'br', 'bg', 'my', 'ca', 'ch', 'ce', 'ny', 'zh', 'cv', 'kw', 'co', 'cr', 'hr', 'cs', 'da', 'dv', 'nl', 'dz', 'en', 'eo', 'et', 'ee', 'fo', 'fj', 'fi', 'fr', 'ff', 'gl', 'ka', 'de', 'el', 'gn', 'gu', 'ht', 'ha', 'he', 'hz', 'hi', 'ho', 'hu', 'ia', 'id', 'ie', 'ga', 'ig', 'ik', 'io', 'is', 'it', 'iu', 'ja', 'jv', 'kl', 'kn', 'kr', 'ks', 'kk', 'km', 'ki', 'rw', 'ky', 'kv', 'kg', 'ko', 'ku', 'kj', 'la', 'lb', 'lg', 'li', 'ln', 'lo', 'lt', 'lu', 'lv', 'gv', 'mk', 'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mh', 'mn', 'na', 'nv', 'nb', 'nd', 'ne', 'ng', 'nn', 'no', 'ii', 'nr', 'oc', 'oj', 'cu', 'om', 'or', 'os', 'pa', 'pi', 'fa', 'pl', 'ps', 'pt', 'qu', 'rm', 'rn', 'ro', 'ru', 'sa', 'sc', 'sd', 'se', 'sm', 'sg', 'sr', 'gd', 'sn', 'si', 'sk', 'sl', 'so', 'st', 'es', 'su', 'sw', 'ss', 'sv', 'ta', 'te', 'tg', 'th', 'ti', 'bo', 'tk', 'tl', 'tn', 'to', 'tr', 'ts', 'tt', 'tw', 'ty', 'ug', 'uk', 'ur', 'uz', 've', 'vi', 'vo', 'wa', 'cy', 'wo', 'fy', 'xh', 'yi', 'yo', 'za', 'zu']
 
 # Read the market place file
-with open("Github-Pages/marketplace.json", "r") as f:
+with open("docs/marketplace.json", "r") as f:
     marketplace = json.load(f)
 
 """ Create new course """
@@ -24,7 +24,7 @@ for mcourse in marketplace:
 
 # Remove course
 if input("Do you want to remove this course ? Yes[Y] or No[N]: ") in ["Yes", "yes", "Y", "y"]:
-    with codecs.open("Github-Pages/marketplace.json", "w", "utf-8") as f:
+    with codecs.open("docs/marketplace.json", "w", "utf-8") as f:
         content = json.dumps([course for course in marketplace if not (course['id'] == courseid)], sort_keys=False, indent=4, separators=(',', ': '))
         f.write(content)
     sys.exit()
@@ -177,11 +177,11 @@ while input("Do you want to add translation ? Yes[Y] or No[N]: ") in ["Yes", "ye
     course["description"][language] = description
 
 if input("Do you want to validate your change ? Yes[Y] or No[N]: ") in ["Yes", "yes", "Y", "y"]:
-    with codecs.open("Github-Pages/marketplace.json", "w", "utf-8") as f:
+    with codecs.open("docs/marketplace.json", "w", "utf-8") as f:
         content = json.dumps(marketplace, sort_keys=False, indent=4, separators=(',', ': '))
         f.write(content)
 else:
     if input("This will lose what you typed! Are you sure ? Yes[Y] or No[N]: ") not in ["Yes", "yes", "Y", "y"]:
-        with codecs.open("Github-Pages/marketplace.json", "w", "utf-8") as f:
+        with codecs.open("docs/marketplace.json", "w", "utf-8") as f:
             content = json.dumps(marketplace, sort_keys=False, indent=4, separators=(',', ': '))
             f.write(content)
